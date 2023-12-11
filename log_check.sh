bfc0170380056b938613c701eee491b8f0d134ea
RominaMorar <106281330+RominaMorar@users.noreply.github.com>
2023-12-11 06:56:03 +0000

    Add files via upload

--- log_check.sh
+++ log_check.sh
@@ -2,17 +2,14 @@
 
 log_file="/var/log/my-app.log"
 
-redis_host="localhost"
-redis_port="6379"
-redis_db="0"
+redis_key="my_app_info"
 
-update_redis() {
-  file_size=$(stat -c %s "$log_file")
-  file_date=$(stat -c %Y "$log_file")
+file_size=$(stat -c %s "$log_file")
+file_date=$(stat -c %Y "$log_file")
 
-  redis-cli -h "$redis_host" -p "$redis_port" -n "$redis_db" SET "log_file_info" "Size: $file_size, Last Modified: $file_date"
-}
-
-if [ "$(stat -c %Y "$log_file")" -ne "$(redis-cli -h "$redis_host" -p "$redis_port" -n "$redis_db" GET "log_file_info" 2>/dev/null)" ]; then
-  update_redis
+if command -v redis-cli &> /dev/null; then
+	redis-cli SET "$redis_key" "{\"size\": $file_size, \"date\": $file_date}"
+	echo "Info successfully written into redis"
+else
+	echo "Error"
 fi
